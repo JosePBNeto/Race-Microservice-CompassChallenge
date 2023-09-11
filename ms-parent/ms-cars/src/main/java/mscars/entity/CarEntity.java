091ae59f3419da0054c0ed831f6ac34d8848cbd1
Jose Patricio Neto <josepatricioneto@hotmail.com>
2023-09-11 18:36:43 -0300

    feature: Car pilot name and car Model must be unique

--- ms-parent/ms-cars/src/main/java/mscars/entity/CarEntity.java
+++ ms-parent/ms-cars/src/main/java/mscars/entity/CarEntity.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
@@ -19,7 +20,9 @@ public class CarEntity {
     @Id
     private String id;
     private String brand;
+    @Indexed(unique = true)
     private String model;
+    @Indexed(unique = true)
     private PilotEntity pilot;
     private String year;
 
--- ms-parent/ms-cars/src/main/java/mscars/exceptions/ControllerAdvice.java
+++ ms-parent/ms-cars/src/main/java/mscars/exceptions/ControllerAdvice.java
@@ -1,9 +1,11 @@
 package mscars.exceptions;
 
+import com.mongodb.MongoWriteException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,6 +17,15 @@ import java.time.LocalDateTime;
 @RestControllerAdvice
 public class ControllerAdvice extends ResponseEntityExceptionHandler {
 
+    CustomResponse customErrorDetails;
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        CustomResponse errorDetails = new CustomResponse(LocalDateTime.now(),
+                ex.getMostSpecificCause().getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         CustomResponse errorDetails = new CustomResponse(LocalDateTime.now(),
@@ -25,9 +36,20 @@ public class ControllerAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(IdNotFoundException.class)
     public ResponseEntity<CustomResponse> handleIdNotFound(IdNotFoundException ex, WebRequest request) {
-        CustomResponse customErrorDetails;
         customErrorDetails = new CustomResponse(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(customErrorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MongoWriteException.class)
+    public ResponseEntity<CustomResponse> handleDuplicateKeyException(MongoWriteException ex, WebRequest request) {
+        customErrorDetails = new CustomResponse(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+
+        if (ex.getMessage().contains("model")) {
+            customErrorDetails = new CustomResponse(LocalDateTime.now(), "Car model already registered", request.getDescription(false));
+        } else if (ex.getMessage().contains("pilot")){
+            customErrorDetails = new CustomResponse(LocalDateTime.now(), "Pilot name already registered", request.getDescription(false));
+        }
+        return new ResponseEntity<>(customErrorDetails, HttpStatus.CONFLICT);
     }
 
 }
