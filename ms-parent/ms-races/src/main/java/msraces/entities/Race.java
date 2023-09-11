9dfdeb1a48eb6775e9e485db4105fca17381cb87
Jose Patricio Neto <josepatricioneto@hotmail.com>
2023-09-11 17:04:44 -0300

    fix: raname API Gateway module

--- ms-parent/ms-races/src/main/java/msraces/entities/Race.java
+++ ms-parent/ms-races/src/main/java/msraces/entities/Race.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
+
 
 import java.util.List;
 
@@ -13,7 +13,6 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 public class Race {
-    @Id
     private String id;
     private Track track;
     private List<Car> cars;
--- ms-parent/ms-races/src/main/java/msraces/repositories/RaceRepository.java
+++
@@ -1,7 +0,0 @@
-package msraces.repositories;
-
-import msraces.entities.Race;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RaceRepository extends MongoRepository<Race, String> {
-}
