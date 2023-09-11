00affe22e4ba35c63eea40ca99480acdf151b33f
Jose Patricio Neto <107478838+JosePBNeto@users.noreply.github.com>
2023-09-10 21:18:57 -0300

    Delete microservices-parent directory

--- microservices-parent/jose.mscars/src/main/java/mscars/repositories/CarRepository.java
+++
@@ -1,7 +0,0 @@
-package mscars.repositories;
-
-import mscars.entity.CarEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CarRepository extends MongoRepository<CarEntity, String> {
-}
