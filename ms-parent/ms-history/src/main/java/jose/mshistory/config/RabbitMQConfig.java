17699b1f6087d7a19060cfa8cd93f6f9b745a7a7
Jose Patricio Neto <josepatricioneto@hotmail.com>
2023-09-12 06:12:40 -0300

    bugfix: Jackson data converter

--- ms-parent/ms-history/src/main/java/jose/mshistory/config/RabbitMQConfig.java
+++ ms-parent/ms-history/src/main/java/jose/mshistory/config/RabbitMQConfig.java
@@ -1,5 +1,7 @@
 package jose.mshistory.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -13,8 +15,14 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    public Jackson2JsonMessageConverter messageConverter(){
-        return new Jackson2JsonMessageConverter();
+    public Jackson2JsonMessageConverter messageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        objectMapper.registerModule(new JavaTimeModule());
+
+        return new Jackson2JsonMessageConverter(objectMapper);
     }
+
+
 
 }
