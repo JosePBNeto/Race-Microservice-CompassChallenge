e279df163155ff9f9202d544c9e2e61b482fbe1e
Jose Patricio Neto <josepatricioneto@hotmail.com>
2023-09-13 06:33:16 -0300

    feature: add Swagger Documentation

--- ms-parent/ms-history/src/main/java/jose/mshistory/MsHistoryApplication.java
+++ ms-parent/ms-history/src/main/java/jose/mshistory/MsHistoryApplication.java
@@ -1,9 +1,22 @@
 package jose.mshistory;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot History Microservice API",
+				description = "Consume a rabbitMQ Queue containing the result of a race and save it to a database",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Jose Patricio Bandeira Barbosa Neto",
+						url = "https://github.com/JosePBNeto"
+				))
+)
 public class MsHistoryApplication {
 
 	public static void main(String[] args) {
